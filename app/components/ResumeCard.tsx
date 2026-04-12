e56bfe30672ad238a8ee4c152e2150591c9cde23
unknown <robpoquin@gmail.com>
2026-04-12 15:53:42 +0200

    Affichage de vrai CV dans la homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import { Link } from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import {usePuterStore} from "~/lib/puter";
 
 // Destructuring des props : on récupère directement les champs de "resume"
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
+    const { fs } = usePuterStore();
+    const [resumeUrl, setResumeUrl] = useState('');
+
+    useEffect(() => {
+        const loadResume = async () => {
+
+            // Lit le fichier (image du CV) via son chemin
+            const blob = await fs.read(imagePath);
+
+            // Sécurité : si aucun fichier trouvé, on stoppe
+            if(!blob) return;
+
+            // Convertit le Blob en URL utilisable et permet d'afficher le fichier directement
+            let url = URL.createObjectURL(blob);
+
+            setResumeUrl(url); // met à jour le state pour affichage
+        }
+
+        loadResume();
+    }, [imagePath]);
+
     return (
         // Route dynamique : chaque carte redirige vers son CV grâce à l'id
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
 
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    <h2 className="!text-black font-bold break-words">{companyName}</h2>
-                    <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+                    {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
+                    {jobTitle && <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>}
+                    {!companyName && !jobTitle && <h2 className="text-black font-bold">CV</h2>}
                 </div>
 
                 <div className="flex-shrink-0">
@@ -20,16 +43,18 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
                 </div>
             </div>
 
+            {resumeUrl && (
             <div className="gradient-border animate-in fade-in duration-1000">
                 <div className="w-full h-full">
                     <img
                         // Image dynamique venant des données
-                        src={imagePath}
+                        src={resumeUrl}
                         alt="CV"
                         className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
                     />
                 </div>
             </div>
+            )}
         </Link>
     )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import { resumes } from "../../constants"; // Import du tableau de CV (données)
 import ResumeCard from "~/components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
-import {useLocation, useNavigate} from "react-router";
-import {useEffect} from "react";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 // Fonction qui définit les métadonnées de la page
 export function meta({}: Route.MetaArgs) {
@@ -16,12 +15,36 @@ export function meta({}: Route.MetaArgs) {
 
 // Composant principal de la page
 export default function Home() {
-    const { auth } = usePuterStore();
+    const { auth, kv } = usePuterStore();
     const navigate = useNavigate();
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false)
 
     useEffect(() => {
+        // Redirige vers la page d'auth si l'utilisateur n'est pas connecté
         if (!auth.isAuthenticated) navigate('/auth?next=/')
     }, [auth.isAuthenticated]);
+
+    useEffect(() => {
+        const loadResumes = async() => {
+            setLoadingResumes(true); // active le loader pendant la récupération
+
+            // Récupère tous les CV stockés avec la clé "resume:*"
+            const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+            // Convertit chaque valeur (string JSON) en objet JS exploitable
+            const parsedResumes = resumes?.map((resume) => (
+                JSON.parse(resume.value) as Resume
+            ))
+
+            console.log("parsedResumes", parsedResumes);
+
+            setResumes(parsedResumes || []); // fallback tableau vide si undefined
+            setLoadingResumes(false); // désactive le loader
+        }
+
+        loadResumes();
+    }, []);
 
   return (
       <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -30,11 +53,20 @@ export default function Home() {
         <section className="main-section">
           <div className="page-heading py-16">
             <h1>Traque tes recherches & Evalue ton CV</h1>
-            <h2>Analyse tes CV et améliore toi avec des retours via IA</h2>
+              {!loadingResumes && resumes?.length === 0 ? (
+                  <h2>Aucun CV trouvé. Importer votre premier CV pour obtenir des conseils</h2>
+              ): (
+                  <h2>Analyse tes CV et améliore toi avec des retours via IA</h2>
+              )}
           </div>
+            {loadingResumes && (
+                <div className="flex flex-col items-center justify-center">
+                    <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+                </div>
+            )}
 
           {/* Affiche la liste des CV seulement s'il y en a */}
-          {resumes.length > 0 && (
+          {!loadingResumes && resumes.length > 0 && (
               <div className="resumes-section">
 
                 {/* Parcours du tableau resumes pour afficher chaque CV */}
@@ -44,10 +76,16 @@ export default function Home() {
                     // key = obligatoire pour React (optimisation du rendu)
                     <ResumeCard key={resume.id} resume={resume} />
                 ))}
-
               </div>
           )}
 
+            {!loadingResumes && resumes?.length === 0 && (
+                <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                    <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                        Importer votre CV
+                    </Link>
+                </div>
+            )}
         </section>
 
       </main>
